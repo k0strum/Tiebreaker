e1db671b8a9974dbd25a14d6b0badf9eac0b3823
Kostrum <k0strumm@gmail.com>
2025-07-24 16:34:13 +0900

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/k0strum/personalProject/PersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k0strum.personalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
