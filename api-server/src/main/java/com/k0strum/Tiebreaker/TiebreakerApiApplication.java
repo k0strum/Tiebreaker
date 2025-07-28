9c71fb16719d4bffce6c4395aad8bc208263529e
kostrum <k0strumm@gmail.com>
2025-07-28 18:19:46 +0900

    프로젝트 확정에 따른 전면 개편

---
+++ api-server/src/main/java/com/k0strum/Tiebreaker/TiebreakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.k0strum.Tiebreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiebreakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiebreakerApiApplication.class, args);
+	}
+
+}
---
+++ api-server/src/test/java/com/k0strum/Tiebreaker/TiebreakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k0strum.Tiebreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiebreakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+} 
